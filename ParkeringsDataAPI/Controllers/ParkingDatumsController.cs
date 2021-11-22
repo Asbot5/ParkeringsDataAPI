41bf5019d1f67dea840fd872e50efe1c365909f1
Ekmul <emil377g@edu.easj.dk>
2021-11-22 10:56:06 +0100

    Add project files.

---
+++ ParkeringsDataAPI/Controllers/ParkingDatumsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkeringsDataAPI.Models;
+
+namespace ParkeringsDataAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ParkingDatumsController : ControllerBase
+    {
+        private ParkeringsdatadbContext _db = new ParkeringsdatadbContext();
+        [HttpGet]
+        public List<ParkingDatum> GetAll()
+        {
+            return _db.ParkingData.ToList();
+        }
+    }
+}
