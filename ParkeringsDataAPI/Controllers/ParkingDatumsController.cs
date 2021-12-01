3580939aed7cb7c6ceeb2febf9f78bf2c2cab8a7
Asmus Skafte Böttcher <asmu0543@edu.easj.dk>
2021-12-01 12:44:52 +0100

    tilføjet filtermetode til specielle parkeringspladser

--- ParkeringsDataAPI/Controllers/ParkingDatumsController.cs
+++ ParkeringsDataAPI/Controllers/ParkingDatumsController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.FileIO;
 using ParkeringsDataAPI.Models;
 
 namespace ParkeringsDataAPI.Controllers
@@ -26,6 +28,26 @@ namespace ParkeringsDataAPI.Controllers
         public List<SpecielleParkeringsPladser> GetAllSpecial()
         {
             return _db.SpecielleParkeringsPladsers.ToList();
+        }
+
+        [Route("Special/{id}")]
+        [HttpGet]
+        public IQueryable GetAllSpecialById(int id)
+        {
+        //    var s = from sp in _db.SpecielleParkeringsPladsers
+        //        where sp.OmrådeId == id
+        //        select new {sp.OmrådeId, sp.OptagedePladser, sp.ParkeringsType, sp.Pladser};
+        //    return s;
+            var s =_db.SpecielleParkeringsPladsers
+                .Where(sp => sp.OmrådeId == id)
+                .Select(sp => new {
+                    id = sp.OmrådeId,
+                    områdeId = sp.OmrådeId,
+                    pladser = sp.Pladser,
+                    optagedePladser = sp.OptagedePladser,
+                    parkeringsType = sp.ParkeringsType
+                });
+                return s;
         }
     }
 }
