4ded883c03da3d651b61357771a7cebb533f4381
AkselSLarsen <akse2669@edu.easj.dk>
2021-12-09 12:09:18 +0100

    Created two SQL scripts to create triggers.

---
+++ SpecielleParkeringsPladserToParkingsområde.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR ALTER TRIGGER SpecielleParkingsPladserToParkingsområde
+ON SpecielleParkeringsPladser
+AFTER UPDATE
+AS BEGIN
+	DECLARE @OmrådeId INT;
+	DECLARE @PreOptagedePladser INT;
+	DECLARE @PostOptagedePladser INT;
+	SELECT @OmrådeId = OmrådeId FROM INSERTED;
+	SELECT @PreOptagedePladser = OptagedePladser FROM DELETED;
+	SELECT @PostOptagedePladser = OptagedePladser FROM INSERTED;
+
+	UPDATE Parkeringsområde
+	SET OptagedePladser = OptagedePladser + (@PostOptagedePladser - @PreOptagedePladser)
+	WHERE Id = @OmrådeId
+END;
